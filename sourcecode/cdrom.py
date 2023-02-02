553d258dddafdf1a4a046372d416cdd5cbd4a243
Klimczus <thisisklimczaaak@gmail.com>
2023-02-02 20:54:48 +0100

    ssd

--- sourcecode/cdrom.py
+++ sourcecode/cdrom.py
@@ -1,7 +1,5 @@
 import ctypes
 import time
 
-# ctypes.windll.WINMM.mciSendStringW(u"open E: type cdaudio alias e_drive", None, 0, None)
-# ctypes.windll.WINMM.mciSendStringW(u"set e_drive door open", None, 0, None)
-
-ctypes.windll.WINMM.mciSendStringW(u"set cdaudio door open", None, 0, None)
+# ctypes.windll.WINMM.mciSendStringW(u"set cdaudio door open", None, 0, None)
+print("Hello")
